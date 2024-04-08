cd10cdc42b6175912e3247706948e226349df72b
ulisesd404 <144632800+ulisesd404@users.noreply.github.com>
2024-04-08 15:12:51 -0700

    schema and some seeds created

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,25 @@
-DROP DATABASE IF EXISTS movies_db;
-CREATE DATABASE movies_db;
+DROP DATABASE IF EXISTS staff_db;
+CREATE DATABASE staff_db;
 
-USE movies_db;
+USE staff_db;
 
-CREATE TABLE movies (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+  dep_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE reviews (
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  role_name VARCHAR(30) NOT NULL
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT,
+);
+
+
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
-    ON DELETE SET NULL
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
 );
