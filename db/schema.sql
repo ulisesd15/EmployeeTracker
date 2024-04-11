9822dd3639d3594516a84bfd3b01b57a65ea5c2d
ulisesd404 <144632800+ulisesd404@users.noreply.github.com>
2024-04-11 15:16:00 -0700

    sql working, nothing showing up yer

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,5 @@
 DROP DATABASE IF EXISTS staff_db;
 CREATE DATABASE staff_db;
-
 USE staff_db;
 
 CREATE TABLE department (
@@ -8,18 +7,20 @@ CREATE TABLE department (
   dep_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE position (
+CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  position_name VARCHAR(30) NOT NULL
-  salary DECIMAL(10, 2) NOT NULL,
+  role_name VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
   dep_id INT,
+  FOREIGN KEY (dep_id) REFERENCES department(id)
 );
-
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  position_id INT,
+  role_id INT,
   manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,24 @@
-INSERT INTO employee (first_name, last_name)
-VALUES  ("Michale","Oliver"),
-        ("Steven", "Smith"),
-        ("Marcelo","Ruiz"),
-        ("Juan","Almaraz"),
-        ("Samantha","Perez");
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Michale","Oliver", 1, 1),
+        ("Gordon","Ramsay", 2, 2),
+        ("Bobby","Flay", 3, 3),
+        ("Guy","Fieri", 4, 4),
+        ("Rachael","Ray", 5, 5),
+        ("Alton","Brown", 6, 6),
+        ("Ina","Garten", 7, 7),
+        ("Giada","De Laurentiis", 8, 8),
+        ("Ree","Drummond", 9, 9),
+        ("Sunny","Anderson", 10, 10),
+        ("Marcela","Valladolid", 11, 11),
+        ("Ayesha","Curry", 12, 12),
+        ("Nigella","Lawson", 13, 13),
+        ("Trisha","Yearwood", 14, 14),
+        ("Paula","Deen", 15, 15),
+        ("Sandra","Lee", 16, 16),
+        ("Alex","Guarnaschelli", 17, 17),
+        ("Anne","Burrell", 18, 18),
 
-INSERT INTO department (dep_name)
+INSERT INTO roles (role_name)
 VALUES ("Produce"),
        ("Cashiers"),
        ("Meat"),
@@ -13,7 +26,7 @@ VALUES ("Produce"),
        ("Bakery");
        ("Janitorial");
 
-INSERT INTO role (role_name, salary, department_id)
+INSERT INTO position (position_name, salary, department_id)
 VALUES  ("Produce Manager", 40000.00, 1),
         ("Cashiers Manager", 40000.00, 2),
         ("Meat Manager", 50000.00, 3),
