456f84b9b60aaa2748710516b3c3e1819b67b916
ulisesd404 <144632800+ulisesd404@users.noreply.github.com>
2024-04-11 19:51:21 -0700

    read functionality working
    :

--- db/query.sql
+++
@@ -1,5 +0,0 @@
-SELECT department.id AS department_id 
-FROM department 
-LEFT JOIN roles ON department.id = roles.department_id 
-LEFT JOIN employee ON roles.id = employee.role_id 
-WHERE employee.manager_id = 1;
